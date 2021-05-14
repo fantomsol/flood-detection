a2c3d71db66c22c9f03168a901be5614cb7a8fb2
DavidRinman <61792198+DavidRinman@users.noreply.github.com>
2021-05-14 15:52:11 +0200

    Ändrat lite i filtreringen, bl.a. gjort den mer användarvänlig (förhoppningsvis)
    - Gjort filtret lättare att ändra. 
    - Lagt till möjlighet att filtrera på olika kategorier. Detta är användbart om man t.ex. vill bara träna med labels som inte är svarta
    - Kommenterat mer

--- utils.py
+++ utils.py
@@ -3,22 +3,39 @@ import os
 import fnmatch
 from PIL import Image
 
-## remove "noise" pictures from directory data_dir
-def purgeNoise(data_dir):
+def _blacklist(img):
+  ### write your logic here to filter out images. 
+  ### Since this is a blacklist, True -> removed
+  ### This method should not be called by the user.
+  median = np.median(img)
+  return median > 254 or median < 1
+  
+def filter(data_dir, category):
+  ### filter function. is called to filter the images in "data_dir"
+  ### based on condition in "_blacklist" applied to the images of "category".
+  ### Category should be one of "flood_label", "vh", "vv" or "water_body_label"]
+  ###
+  ### NOTE that this function deletes images from your file system. Back up your data
+  ### if you don't want to download it again to restore filtering.
+  
+  cats = ["flood_label", "vh", "vv", "water_body_label"]
+  if not category in cats:
+    print("Invadlid category. Please enter one of the following:")
+    print(cats)
+    return -1
   print("Indexing files...")
   rm_paths = []
-  cats = ["flood_label", "vh", "vv", "water_body_label"]
   for setname in os.listdir(data_dir):
     idx = []
     tiles = os.path.join(data_dir, setname, "tiles")
     categories = [os.path.join(tiles, c) for c in cats]
-    files = os.listdir(categories[1])
+    files = os.listdir(category)
     files = fnmatch.filter(files, "*.png")
     
     for i, f in enumerate(files):
       path = os.path.join(categories[1], f)
       img = Image.open(path)
-      if np.median(img) > 254:
+      if _blacklist(img):
         idx.append(i)
     print(categories[1] + ": " + f"{len(idx)}" + " of " + f"{len(files)}" + " noise.")
     
@@ -39,4 +56,4 @@ def purgeNoise(data_dir):
     print("Done.")
   else:
     print("Abort.")
-  
+
