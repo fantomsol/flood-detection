c32e3e2e67d15a97c660a77013a227488ef6a533
erisochalmers <erik.soderberg.us@gmail.com>
2021-05-14 13:23:54 +0200

    img test etc

--- unet.py
+++ unet.py
@@ -2,30 +2,9 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import backend as K
 import numpy as np
-
+from davids_kod import DataSplitter, BatchGenerator
 
 def main():
-    # custom weighted binary crossentropy on 2d output
-    # higher weight means higher penalty for getting 1's wrong
-    def weighted_2d_bce(weight):
-        # input dims: [batch_size, img_width, img_height, 1]
-        # output dims: [batch_size,]
-        def loss_func(true, pred):
-            # avoid inf/nan, more stable numerics
-            pred = K.clip(pred, K.epsilon(), 1 - K.epsilon())
-
-            # first term when label == 1, scale loss contribution by weight
-            loss = weight * true * K.log(pred) + (1 - true) * K.log(1 - pred)
-
-            # remove redundant dimension and sum up 2d array
-            loss = K.squeeze(loss, -1)
-            loss = K.sum(loss, -1)
-            loss = K.sum(loss, -1)
-
-            # return negated value
-            return -loss
-
-        return loss_func
 
 
     # note: removed dropout between the double-convs. add again if overfitting becomes a problem
@@ -102,8 +81,79 @@ def main():
     # should perhaps be done in preprocessing
     train_y = np.divide(train_y, 255)
 
+    dataSplitter = DataSplitter("tiles", batch_size=16, validation_split=0.2)
+    train_gen, val_gen = dataSplitter.getBatchGenerators()
+
+
     model = unet(INPUT_DIMS, class_weight_bias=20)
-    model.fit(train_x, train_y, validation_split=0.1, batch_size=8, epochs=1)
+    model.fit(train_gen, validation_data=val_gen, epochs=2)
+
+    model.save("my_model.h5")
+
+
+def test_network():
+
+    model = tf.keras.models.load_model('my_model.h5', custom_objects={'loss_func': loss_func})
+    train_x = np.load("train_x.npy")
+    train_y = np.load("train_y.npy")
+
+    n = 10
+    pred = model(train_x[0:n,:,:,:], training=False)
+    pred = pred.numpy()
+
+
+    for i in range(n):
+        fig = plt.figure(figsize=(8, 4))
+
+        fig.add_subplot(1, 2, 1)
+        plt.imshow(train_x[i, :, :, 0], cmap="gray")
+
+        fig.add_subplot(1, 2, 2)
+        plt.imshow(pred[i, :, :, 0], cmap="gray")
+
+        plt.show()
+
+
+
+# custom weighted binary crossentropy on 2d output
+# higher weight means higher penalty for getting 1's wrong
+def weighted_2d_bce(weight):
+    # input dims: [batch_size, img_width, img_height, 1]
+    # output dims: [batch_size,]
+    def loss_func(true, pred):
+        # avoid inf/nan, more stable numerics
+        pred = K.clip(pred, K.epsilon(), 1 - K.epsilon())
+
+        # first term when label == 1, scale loss contribution by weight
+        loss = weight * true * K.log(pred) + (1 - true) * K.log(1 - pred)
+
+        # remove redundant dimension and sum up 2d array
+        loss = K.squeeze(loss, -1)
+        loss = K.sum(loss, -1)
+        loss = K.sum(loss, -1)
+
+        # return negated value
+        return -loss
+
+    return loss_func
+
+
+
+WEIGHT = 20
+def loss_func(true, pred):
+    # avoid inf/nan, more stable numerics
+    pred = K.clip(pred, K.epsilon(), 1 - K.epsilon())
+
+    # first term when label == 1, scale loss contribution by weight
+    loss = WEIGHT * true * K.log(pred) + (1 - true) * K.log(1 - pred)
+
+    # remove redundant dimension and sum up 2d array
+    loss = K.squeeze(loss, -1)
+    loss = K.sum(loss, -1)
+    loss = K.sum(loss, -1)
+
+    # return negated value
+    return -loss
 
 # sample a number of images/labels from the dataset
 # concat vv and vh along channel dimension
@@ -131,4 +181,8 @@ def create_subsample(n_samples):
 
 if __name__ == '__main__':
     # create_subsample(5000)
-    main()
+    # main()
+    test_network()
+
+
+
